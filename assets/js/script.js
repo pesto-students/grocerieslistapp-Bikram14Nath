d812730a43ee6af1d1060541c4994f5f350c0547
Bikram Nath <bikram.1795@gmail.com>
2020-12-26 17:48:45 +0530

    add and restore

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,115 @@
-document.getElementById('user_save_btn').onclick = function () {
-    let user_name = document.getElementById('userName').value;
-    checkUser(user_name);
-}
+const final = 5;
+var myitem = 0;
+document.getElementById("user_save_btn").onclick = function () {
+  let user_name = document.getElementById("userName").value;
+  checkUser(user_name);
+};
 let groceryItems = [];
+let groceryList = JSON.parse(localStorage.getItem("groceryList"));
+if (groceryList === null) {
+  groceryList = {};
+}
+let user = "";
 
 function checkUser(name) {
-    let users = [];
-    let userData = localStorage.getItem('userData');
-    if (userData !== null) {
-        users = userData.split(',');
-    }
-    if (users.length > 0) {
-        if (users.includes(name)) {
-            const items = getItemListByUser(name);
-        } else {
-            users.push(name);
-            localStorage.setItem('userData', users.toString());
-        }
+  user = name;
+  localStorage.setItem("currentUser", user);
+  let users = [];
+  let userData = localStorage.getItem("userData");
+  if (userData !== null) {
+    users = userData.split(",");
+  }
+  if (users.length > 0) {
+    if (users.includes(name)) {
+      this.getItemListByUser(name);
     } else {
-        users.push(name);
-        localStorage.setItem('userData', users.toString());
+      users.push(name);
+      localStorage.setItem("userData", users.toString());
+      groceryList[name] = [];
+      groceryItems = groceryList[name];
+      console.log(groceryList[name]);
+      console.log(groceryList);
     }
-    document.getElementById('login-form').classList.add('hide');
-    document.getElementById('item-list').classList.remove('hide');
+  } else {
+    users.push(name);
+    localStorage.setItem("userData", users.toString());
+    groceryList[name] = [];
+    console.log(groceryList[name]);
+    groceryItems = groceryList[name];
+    console.log(groceryList);
+  }
+  document.getElementById("login-form").classList.add("hide");
+  document.getElementById("item-list").classList.remove("hide");
 }
-function getItemListByUser(name){
-    let groceryList = JSON.parse( localStorage.getItem('groceryList'));
-    let myList = [];
-    if (groceryList !== null && groceryList[name]) {
-        myList = groceryList[name];
-    }
-    console.log(myList);
-    return myList;
-    // else {
-    //     groceryList = {};
-    //     groceryList[name] = [];
-    // }
-    // groceryList[name].push('pudina');
-    // localStorage.setItem('groceryList', JSON.stringify(groceryList))
+function getItemListByUser(name) {
+  if (groceryList !== null && groceryList[name]) {
+    groceryItems = groceryList[name];
+  } else {
+    groceryList[name] = [];
+    groceryItems = groceryList[name];
+  }
+  this.renderListItems(groceryItems);
 }
-renderGroceryItems(){
+function addToBasket(item) {
+  groceryList[user].push(item);
+  console.log(groceryList);
+  localStorage.setItem("groceryList", JSON.stringify(groceryList));
+}
 
+function removeFromBasket(item) {
+  const indexItem = groceryList[user].indexOf(item);
+  console.log(indexItem);
+}
+
+function renderListItems(myGroceryItems) {
+  myitem = myGroceryItems.length;
+  console.log(myitem === final);
+  if (myitem === final) {
+    document.getElementById("addbtn").classList.add("hide");
+  }
+  for (let index = 0; index < myGroceryItems.length; index++) {
+    var li = document.createElement("li");
+    var t = document.createTextNode(myGroceryItems[index]);
+    li.appendChild(t);
+    document.getElementById("myUL").appendChild(li);
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+  }
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
+}
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === "") {
+    alert("Enter a valid item!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+    this.addToBasket(inputValue);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+  ++myitem;
+  if (myitem === final) {
+    document.getElementById("addbtn").classList.add("hide");
+  }
 }
